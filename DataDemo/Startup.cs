f84ff0b428229eae64ce9434d6aaf718fd15e1fd
codenamejason <codenamejason@outlook.com>
2015-12-09 21:11:38 -0500

    initial project commit.

---
+++ DataDemo/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DataDemo.Startup))]
+namespace DataDemo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
